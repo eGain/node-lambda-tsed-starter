926931b9e961c40984e1d5e5ef8acb8865df2012
ygyg70 <yonatan.goraly@gmail.com>
2024-04-03 23:28:37 -0700

    feat: work on implementing some additional scenarios

--- packages/controllers/src/hello/HelloController.ts
+++ packages/controllers/src/hello/HelloController.ts
@@ -2,18 +2,20 @@ import { GreetingRequest } from "@project/domain/hello/GreetingRequest.js"
 import { HelloService } from "@project/service/hello/HelloService.js"
 import { Controller, Inject } from "@tsed/di"
 import { BodyParams, QueryParams } from "@tsed/platform-params"
-import { Returns } from "@tsed/schema"
+import { Get, Post, Returns } from "@tsed/schema"
 
 @Controller("/hello")
 export class HelloController {
   @Inject()
   private helloService: HelloService
 
+  @Get("/")
   @Returns(200, String)
   get(@QueryParams("name") name: string) {
     return `Hello ${name}!`
   }
 
+  @Post("/")
   @Returns(200, Array<string>)
   post(@BodyParams() payload: GreetingRequest) {
     return this.helloService.handleGreetRequest(payload)
