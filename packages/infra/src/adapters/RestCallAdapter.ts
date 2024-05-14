8552bed65732c1805d54386a2f72a16f8f227f93
ygyg70 <yonatan.goraly@gmail.com>
2024-05-14 09:51:40 -0700

    Mock server as a testcontainer POC

--- packages/infra/src/adapters/RestCallAdapter.ts
+++ packages/infra/src/adapters/RestCallAdapter.ts
@@ -1,10 +1,14 @@
-import axios from "axios"
+import axios, { AxiosInstance } from "axios"
 
 export class RestCallAdapter {
-  private axiosInstance = axios.create({
-    baseURL: "http://localhost:1080",
-    timeout: 60000
-  })
+  private axiosInstance: AxiosInstance
+
+  constructor(baseUrl: string) {
+    this.axiosInstance = axios.create({
+      baseURL: baseUrl,
+      timeout: 60000
+    })
+  }
 
   async simpleGet<T>(path: string): Promise<T> {
     const response = await this.axiosInstance.get<T>(path)
